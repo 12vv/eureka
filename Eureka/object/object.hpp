7b8768aa2d82575a13283f486d25bec3c252c4f1
garrick <yegarrick12@gmail.com>
2020-01-26 20:52:47 +0800

    add sphere

--- Eureka/object/object.hpp
+++ Eureka/object/object.hpp
@@ -11,6 +11,25 @@
 
 #include "../utility/global.hpp"
 
+
+bool solveQuadratic(const float &a, const float &b, const float &c, float &x0, float &x1)
+{
+    float discr = b * b - 4 * a * c;
+    if (discr < 0) return false;
+    else if (discr == 0) {
+        x0 = x1 = - 0.5 * b / a;
+    }
+    else {
+        float q = (b > 0) ?
+        -0.5 * (b + sqrt(discr)) :
+        -0.5 * (b - sqrt(discr));
+        x0 = q / a;
+        x1 = c / q;
+    }
+    
+    return true;
+}
+
 class Object {
 public:
     Object() : color(dis(gen), dis(gen), dis(gen)) {}
--- Eureka/object/sphere.hpp
+++ Eureka/object/sphere.hpp
@@ -9,11 +9,38 @@
 #ifndef sphere_h
 #define sphere_h
 
-#include "sphere.hpp"
+#include "object.hpp"
 
-class Sphere {
+class Sphere : public Object {
 public:
     Sphere(const Vec3f &c, const float &r): radius(r), radius2(r*r), center(c) {}
+    // Ray-sphere intersection test
+    // @param ori: the ray origin
+    // @param dir: the ray direction
+    // @param t: the distance from the ray origin to tht intersection point
+    virtual bool intersect(const Vec3f &ori, const Vec3f &dir, float &t) const {
+        float t0, t1; // solutions for t if the ray intersects
+        
+        // dot((p(t)-C), (p(t)-C)) = R^2 ;
+        // p(t) = origin + t * direction ;
+        // t^2 * dot(dir, dir) + 2*t*dot(dir, (origin-C)) + dot((origin-C), (origin-C)) - R^2 = 0 ;
+        Vec3f L = ori - center;
+        float a = dir.dotProduct(dir);
+        float b = 2 * dir.dotProduct(L);
+        float c = L.dotProduct(L) - radius2;
+        if (!solveQuadratic(a, b, c, t0, t1)) return false;
+        
+        if (t0 > t1) std::swap(t0, t1);
+        
+        if (t0 < 0) {
+            t0 = t1; // if t0 is negative, let's use t1 instead
+            if (t0 < 0) return false; // both t0 and t1 are negative
+        }
+        
+        t = t0;
+        
+        return true; 
+    }
     
     float radius, radius2;
     Vec3f center;
